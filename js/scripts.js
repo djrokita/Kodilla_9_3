b38cdc34b12e9ec1d5b8a82316f3bb180ac08cc0
Krzych <krzych.pierzchala@gmail.com>
2017-09-27 13:58:16 +0200

    Fixed js file - added mising semicolon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.'
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
 
 var dinosaur = 'triceratops';
 
